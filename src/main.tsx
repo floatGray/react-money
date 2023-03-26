7b3d062a918fe584bb114c0a5fc1bdd236abf69c
floatgray <599316783@qq.com>
2023-03-26 10:16:09 +0800

    初始化路由，添加404

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,19 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { App } from "./App";
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import { ErrorPage } from './components/ErrorPage'
 
-const div = document.getElementById("root") as HTMLElement;
-const root = ReactDOM.createRoot(div);
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <div>root</div>,
+    errorElement: <ErrorPage />,
+  },
+])
+const div = document.getElementById('root') as HTMLElement
+const root = ReactDOM.createRoot(div)
 root.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+    <RouterProvider router={router} />
+  </React.StrictMode>,
+)
