6714292a417bcc0561703729a0f7bef7489a2e5a
floatgray <599316783@qq.com>
2023-03-27 13:37:31 +0800

    优化组件

--- src/routes/router.tsx
+++ src/routes/router.tsx
@@ -1,23 +1,23 @@
-import { Outlet, createBrowserRouter } from 'react-router-dom'
-import { NotFoundPage } from '../pages/NotFoundPage'
-import { MainLayout } from '../layouts/MainLayout'
-import { WelcomeRoutes } from './welcomeRoutes'
+import { createBrowserRouter } from 'react-router-dom'
+import { Root } from '../layouts/Root'
+import { WelcomeLayout } from '../layouts/WelcomeLayout'
+import { Welcome1 } from '../pages/Welcome1'
+import { Welcome2 } from '../pages/Welcome2'
+import { Welcome3 } from '../pages/Welcome3'
+import { Welcome4 } from '../pages/Welcome4'
 
 export const router = createBrowserRouter([
+  { path: '/', element: <Root/> },
+  { path: '/home', element: <div>home</div> },
   {
-    path: '/home',
-    element: <div>home</div>,
-  },
-  {
-    path: '/',
-    element: <MainLayout/>,
-  },
-  {
-    path: '/',
-    element: <Outlet/>,
-    errorElement: <NotFoundPage />,
+    path: '/welcome',
+    element: <WelcomeLayout/>,
     children: [
-      WelcomeRoutes,
+      { index: true, element: <Welcome1/> },
+      { path: '1', element: <Welcome1/> },
+      { path: '2', element: <Welcome2/> },
+      { path: '3', element: <Welcome3/> },
+      { path: '4', element: <Welcome4/> },
     ],
   },
 ],
