da37e57a4b8606bf833246694b3ac06c49cdb7f5
floatgray <599316783@qq.com>
2023-03-28 14:46:40 +0800

    封装icon

--- src/components/Icon.tsx
+++ src/components/Icon.tsx
@@ -1,3 +1,14 @@
-export const Icon: React.FC = () => {
-  return <div>Icon </div>
+import classname from 'classnames'
+import style from './Icon.module.scss'
+interface Props {
+  name: string
+  className?: string
+}
+
+export const Icon: React.FC<Props> = ({ name, className }) => {
+  return (
+    <svg className={classname(className, style.icon)}>
+      <use xlinkHref={`#${name}`}></use>
+    </svg>
+  )
 }
