ad594e091d27c0314bc93097aa0912bacef4a683
floatgray <599316783@qq.com>
2023-03-28 16:07:09 +0800

    完成TimeRangePicker组件

---
+++ src/components/TopNav.tsx
@@ -0,0 +1,14 @@
+import { Icon } from './Icon'
+
+interface Props {
+  title?: string
+}
+
+export const TopNav: React.FC<Props> = ({ title = '记账本' }) => {
+  return (
+    <div text-white flex items-center pt-24px pb-8px px-24px>
+    <Icon name="menu" className="w-24px h-24px mr-16px" />
+    <h1 text-24px>{title}</h1>
+  </div>
+  )
+}
---
+++ src/pages/ItemsPage.tsx
@@ -0,0 +1,27 @@
+import { useState } from 'react'
+import styled from 'styled-components'
+import { AddItemFloatButtun } from '../components/AddItemFloatButton'
+import type { TimeRange } from '../components/TimeRangePicker'
+import { TimeRangePicker } from '../components/TimeRangePicker'
+import { TopNav } from '../components/TopNav'
+import { ItemsList } from './ItemsPage/ItemsList'
+import { ItemsSummary } from './ItemsPage/ItemsSummary'
+
+const Div = styled.div`
+  background: linear-gradient(0deg, rgba(143,76,215,1) 0%, rgba(92,51,190,1) 100%);
+`
+
+export const ItemsPage: React.FC = () => {
+  const [timeRange, setTimeRange] = useState<TimeRange>('thisMonth')
+  return (
+  <div>
+    <Div>
+        <TopNav/>
+        <TimeRangePicker selected={timeRange} onSelected={setTimeRange}/>
+    </Div>
+    <ItemsSummary/>
+    <ItemsList/>
+    <AddItemFloatButtun/>
+  </div>
+  )
+}
