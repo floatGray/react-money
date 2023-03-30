4fe4221ae015a1d49e62d530d9eb6feb03692c29
floatgray <599316783@qq.com>
2023-03-30 13:58:13 +0800

    重构组件代码，添加注册页面

--- src/routes/router.tsx
+++ src/routes/router.tsx
@@ -3,13 +3,14 @@ import { Root } from '../components/Root'
 import { WelcomeLayout } from '../layouts/WelcomeLayout'
 import { Home } from '../pages/Home'
 import { ItemsPage } from '../pages/ItemsPage'
+import { SignInPage } from '../pages/SignInPage'
 import { Welcome1 } from '../pages/Welcome1'
 import { Welcome2 } from '../pages/Welcome2'
 import { Welcome3 } from '../pages/Welcome3'
 import { Welcome4 } from '../pages/Welcome4'
 
 export const router = createBrowserRouter([
-  { path: '/', element: <Root />, },
+  { path: '/', element: <Root /> },
   { path: '/home', element: <Home title="首页" /> },
   {
     path: '/welcome',
@@ -19,10 +20,10 @@ export const router = createBrowserRouter([
       { path: '2', element: <Welcome2 /> },
       { path: '3', element: <Welcome3 /> },
       { path: '4', element: <Welcome4 /> },
-    ]
+    ],
   },
   { path: '/items', element: <ItemsPage /> },
-  { path: '/sign_in', element: <div>sign in</div> },
+  { path: '/sign_in', element: <SignInPage /> },
   { path: '/chart', element: <div>图表</div> },
   { path: '/export', element: <div>敬请期待</div> },
   { path: '/tags', element: <div>标签</div> },
