aa5ae3d9c112e3bbe94927a180839f2b5bdd9b5a
floatgray <599316783@qq.com>
2023-04-03 12:51:16 +0800

    添加选择年份时候可以对齐选择框的功能

--- src/pages/ItemsNewPage/DateAndAmount.tsx
+++ src/pages/ItemsNewPage/DateAndAmount.tsx
@@ -24,8 +24,15 @@ export const DateAndAmount: React.FC<Props> = (props) => {
       }
     }}
     onTouchEnd={() => {
+      const remainder = translateY % 36
+      let y = translateY - remainder
+      if (Math.abs(remainder) > 18) {
+        y += 36 * (remainder > 0 ? 1 : -1)
+      }
+      setTranslateY(y)
       setIsTouching(false)
-    }}
+    }
+    }
   >
     <div b-1 b-red h-36px absolute top="[calc(50%-18px)]" w-full />
     <div b-1 b-red h-36px absolute top="[calc(50%-18px)]" w-full>
@@ -42,17 +49,19 @@ export const DateAndAmount: React.FC<Props> = (props) => {
         <li>2008</li>
         <li>2009</li>
         <li>2010</li>
-        <li>2000</li>
-        <li>2001</li>
-        <li>2002</li>
-        <li>2003</li>
-        <li>2004</li>
-        <li>2005</li>
-        <li>2006</li>
-        <li>2007</li>
-        <li>2008</li>
-        <li>2009</li>
-        <li>2010</li>
+        <li>2011</li>
+        <li>2012</li>
+        <li>2013</li>
+        <li>2014</li>
+        <li>2015</li>
+        <li>2016</li>
+        <li>2017</li>
+        <li>2018</li>
+        <li>2019</li>
+        <li>2020</li>
+        <li>2021</li>
+        <li>2022</li>
+        <li>2023</li>
       </ol>
     </div>
   </div>)
