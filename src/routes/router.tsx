f9a6eb52b1d5dcf1ce647beab13bef6f39b3b797
floatgray <599316783@qq.com>
2023-04-03 15:34:46 +0800

    添加数字修改显示

--- src/routes/router.tsx
+++ src/routes/router.tsx
@@ -5,6 +5,7 @@ import { Home } from '../pages/Home'
 import { ItemsNewPage } from '../pages/ItemsNewPage'
 import { ItemsPage } from '../pages/ItemsPage'
 import { SignInPage } from '../pages/SignInPage'
+import { TagsNewPage } from '../pages/TagsNewPage'
 import { Welcome1 } from '../pages/Welcome1'
 import { Welcome2 } from '../pages/Welcome2'
 import { Welcome3 } from '../pages/Welcome3'
@@ -26,6 +27,7 @@ export const router = createBrowserRouter([
   { path: '/items', element: <ItemsPage /> },
   { path: '/items/new', element: <ItemsNewPage /> },
   { path: '/sign_in', element: <SignInPage /> },
+  { path: '/tags/new', element: <TagsNewPage /> },
   { path: '/chart', element: <div>图表</div> },
   { path: '/export', element: <div>敬请期待</div> },
   { path: '/tags', element: <div>标签</div> },
