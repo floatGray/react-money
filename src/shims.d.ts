0bfd7f29182e6d1f0f245b4567c656abd256c564
floatgray <599316783@qq.com>
2023-04-06 11:01:19 +0800

    修改Popup组件，支持Loading

--- src/shims.d.ts
+++ src/shims.d.ts
@@ -19,6 +19,7 @@ declare module 'react' {
     'focus:shadow'?: boolean
     absolute?: boolean
     top?: string
+    left?: string
   }
   interface SVGProps<T> extends SVGAttributes<T>, ClassAttributes<T> {
     w?: string
