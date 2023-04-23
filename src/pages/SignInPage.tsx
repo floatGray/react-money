2b4e5fbcb417f74dc47d1e6e67f0d2296c931b96
floatgray <599316783@qq.com>
2023-04-23 21:18:34 +0800

    删除写死的后端

--- src/pages/SignInPage.tsx
+++ src/pages/SignInPage.tsx
@@ -29,7 +29,7 @@ export const SignInPage: React.FC = () => {
     setError(newError)
     if (!hasError(newError)) {
       // 发送请求
-      const response = await post<{ jwt: string }>('https://mangosteen2.hunger-valley.com/api/v1/session', data)
+      const response = await post<{ jwt: string }>('/api/v1/session', data)
         .catch(onSubmitError)
       // 获取 JWT
       const jwt = response.data.jwt
@@ -46,7 +46,7 @@ export const SignInPage: React.FC = () => {
     ])
     setError(newError)
     if (hasError(newError)) { throw new Error('表单出错') }
-    const response = await post('http://121.196.236.94:8080/api/v1/validation_codes', {
+    const response = await post('/api/v1/validation_codes', {
       email: data.email
     })
     return response
